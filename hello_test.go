dbe94807c5bbec2d0007378e25bc25d06ba956d0
0xk3v <kevin_@tuta.io>
2024-07-12 00:05:32 +0200

    refactored tests and removed duplicates

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,20 @@ func TestHello(t *testing.T) {
 		got := Hello("John")
 		want := "Hello, John"
 
-		if got != want {
-			t.Errorf("got %q, wanted %q ", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying 'Hello world' when empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("got %q, wanted %q ", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, wanted %q ", got, want)
+	}
 }
