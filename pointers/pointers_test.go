6113859703536c0258f23c87a49ac80b444e21fc
0xk3v <kevin_@tuta.io>
2024-09-06 14:30:18 +0200

    added tests for Withdraw method

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -3,14 +3,32 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("Testing Deposit", func(t *testing.T) {
+		// Initializing wallet
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
+		// Operations
+		wallet.Deposit(10)
+		got := wallet.Balance()
+		want := 10
 
-	got := wallet.Balance()
-	want := 10
+		if got != want {
+			t.Errorf("Got %d, want %d", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("Got %d, want %d", got, want)
-	}
+	t.Run("Testing Withdraw", func(t *testing.T) {
+		// Setting up Wallets
+		wallet := Wallet{}
+		wallet.Deposit(200)
+
+		wallet.Withdraw(50)
+
+		got := wallet.Balance()
+		want := 150
+
+		if got != want {
+			t.Errorf("Got %d, want %d", got, want)
+		}
+	})
 }
