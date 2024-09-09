3c7d32a66d87a8b4bb6d912c6d2800cfe95cd892
0xk3v <kevin_@tuta.io>
2024-09-09 22:52:00 +0200

    added new types and methods

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,22 +1,31 @@
 package pointers
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC ", b)
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Withdraw(amount int) error {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
-		return errors.New("invalid amount")
+		return errors.New("cannot withdraw, insufficient funds")
 	}
 
 	w.balance -= amount
